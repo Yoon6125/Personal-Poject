9368367743382ef7f1c446e7249a59fc957a2ccc
Heegeun.Yoon <58425997+Yoon6125@users.noreply.github.com>
2024-09-06 17:49:50 +0900

    회원가입 & 회원의 캐릭터 구현

---
+++ RPG_Project(MUD textgame)/v.0.0.2/sql 데이터/my_cat_rpg_memberanduser.sql
@@ -0,0 +1,50 @@
+-- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
+--
+-- Host: localhost    Database: my_cat
+-- ------------------------------------------------------
+-- Server version	8.0.32
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `rpg_memberanduser`
+--
+
+DROP TABLE IF EXISTS `rpg_memberanduser`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `rpg_memberanduser` (
+  `id_member` char(8) NOT NULL,
+  `c_name` char(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `rpg_memberanduser`
+--
+
+LOCK TABLES `rpg_memberanduser` WRITE;
+/*!40000 ALTER TABLE `rpg_memberanduser` DISABLE KEYS */;
+INSERT INTO `rpg_memberanduser` VALUES ('아이디','테스트100'),('테스트계정','테스트기사'),('테스트계정','테스트보병'),('테스트계정','테스트사수'),('아이디','드디어성공'),('아이디','코딩개어려움'),('아이디','최강사수봉진호'),('아이디','슈팅바ㅋ간');
+/*!40000 ALTER TABLE `rpg_memberanduser` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-09-06 17:47:13
